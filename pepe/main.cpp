8d99d9b1c842befc1696cb8c05dd9abe3b61ae2a
Troniks1SZR <gmhtronik@hotmail.com>
2022-05-05 15:57:10 +0000

    Conseguido

---
+++ pepe/main.cpp
@@ -0,0 +1,75 @@
+#include<fstream>
+#include<iostream>
+using std::cout;
+#include<string>
+using std::string;
+
+
+std::string deleteWhiteSpacesSurround(std::string str);
+
+int main(void){
+    string reempN = "Negative,";
+    string reempN_con = "\nNegative,";
+    string reempP = "Positive,";
+    string reempP_con = "\nPositive,";
+    string line;
+    size_t lenN = reempN.length();
+    size_t lenP = reempP.length();
+
+    std::ifstream archivo_in{ "pepe.txt" };//archivo de entrada
+    std::ofstream archivo_out{ "temp.txt" };//archivo de salida
+
+    if (archivo_in && archivo_out) {//si los archivos fueron abiertos correctamente dara true
+        
+       int contador = 0;
+  std::string line;
+  std::string tweet = "";
+  while (std::getline(archivo_in, line)) {
+    line = deleteWhiteSpacesSurround(line);
+    if (line.empty()) {
+      continue;
+    }
+    if(contador != 0){
+        size_t pos;
+        if((pos = line.find(reempN) != string::npos)){
+                line.replace(pos, lenN, reempN_con);//si la encuentra la reemplaza con México
+        }
+        else if((pos = line.find(reempP) != string::npos)){
+            line.replace(pos, lenP, reempP_con);//si la encuentra la reemplaza con México
+        }
+    }
+      tweet += line + " ";
+      contador = 1;
+    }
+    archivo_out << tweet << '\n';   
+        
+        
+        
+        /*while (getline(archivo_in, line)){//lee una línea
+            while (true){
+                size_t pos = line.find(reemp);//busca méxico en la línea actual
+                if (pos != string::npos)
+                    line.replace(pos, len, reemp_con);//si la encuentra la reemplaza con México
+                else
+                    break;
+            }
+            archivo_out << line << '\n';//aqui escribe la línea corregida en el nuevo archivo
+        }*/
+    }
+    else
+        cout << "No se pueden abrir los archivos";
+}
+
+
+std::string deleteWhiteSpacesSurround(std::string str) {
+  while (!str.empty()) {
+    if ((str[0] == ' ') || (str[0] == '\t') || ((int)str[0] == 13) || (str[0] == '"')) {
+      str.erase(str.begin());
+    } else if ((str[str.size() - 1] == ' ')  || (str[str.size() - 1] == '\t') || ((int)str[str.size() - 1] == 13) || (str[str.size() - 1] == '"')) {
+      str.erase(str.size() - 1);
+    } else {
+      break;
+    }
+  }
+  return str;
+}
